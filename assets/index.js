593ac39c0a7e8b1125bdf546f030231a371bcadc
unknown <jamesqraad@gmail.com>
2021-01-07 19:26:36 +1100

    css update

--- assets/index.js
+++ assets/index.js
@@ -39,13 +39,3 @@ function queryString(originalString) {
 }
 init();
 
-//$(function() {
-	//$(window).scroll(function() {
-	  //var winTop = $(window).scrollTop();
-	  //if (winTop >= 30) {
-		//$("body").addClass("sticky-shrinknav-wrapper");
-	 // } else{
-	//	$("body").removeClass("sticky-shrinknav-wrapper");
-	  //}
-	//});
- // });
